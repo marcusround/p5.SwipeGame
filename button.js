1da9fad77ba406544a3d95b8b762195688577413
Marcus Round <marcusround@gmail.com>
2020-08-27 13:14:22 +0800

    Positive/negative previews

--- button.js
+++ button.js
@@ -2,25 +2,58 @@ class Button {
 
     constructor ( p ) {
 
+        this.id = p.id;
+
         this.x = p.x || 0;
         this.y = p.y || 0;
         this.radius = p.radius || 40;
 
         this.colour = p.colour || 'white';
 
-        this.clickEffect = p.clickEffect;
+        this.OnClick = p.OnClick;
+        this.OnHover = p.OnHover;
+        this.OnHoverExit = p.OnHoverExit;
+        this.hoverState = false;
         
     }
 
-    Click () {
+    EvaluateMousePress ( x, y ) {
 
-        if (this.clickEffect) {
-            this.clickEffect()
+        if (this.OnClick && this.IsUnderneathPoint(x,y)) {
+            this.OnClick();
         }
         
     }
 
-    IsWithinRange ( x, y ) {
+    EvaluateMousePosition ( x, y ) {
+
+        if ( this.IsUnderneathPoint( x, y ) ) {
+
+            if ( this.hoverState === false ) {
+
+                this.hoverState = true;
+                if (this.OnHover) {
+                    this.OnHover();
+                }
+                
+            }
+
+        } else {
+
+            if ( this.hoverState === true ) {
+
+                this.hoverState = false;
+                if (this.OnHoverExit) {
+                    this.OnHoverExit();
+                }
+
+            }
+
+        }
+        
+    }
+
+    IsUnderneathPoint ( x, y ) {
 
         return dist(x, y, this.x, this.y) < this.radius;
         
--- sketch.js
+++ sketch.js
@@ -9,6 +9,161 @@ const buttons = [];
 
 const factorCount = 4;
 const factors = [];
+const factorBars = [];
+const factorNames = ["Lorem", "Ipsum", "Dolor", "Sit amet"];
+
+const palette = {
+
+  'background': '#456789',
+  'background-2': '#99eaea',
+  'positive': '#11d211',
+  'negative': '#e32222',
+  'foreground': '#00cccc',
+  'highlight': '#ffd633',
+  'ui': '#eeeeee',
+  'black': '#123456',
+
+}
+
+function applyEffects(card, choice) {
+
+  const effects = card.effects[choice];
+  
+  for (let i = 0; i < effects.length; i++) {
+    
+    factors[i].ApplyEffect(effects[i]);
+    
+  }
+  
+}
+
+function clearPreviews() {
+  
+  factorBars.forEach(bar => {
+    bar.SetPreview(0);
+  })
+  
+}
+
+function downloadExampleCSV() {
+  
+  saveTable(exampleCSV, 'swipe_game_example.csv', 'csv');
+  
+}
+
+function draw() {
+
+  if (deck.stack.length === 0) {
+    background(23, 45, 67);
+    return;
+  }
+  
+  background(palette['background']);
+  
+  // Draw the active card to screen
+  if (activeCard && activeCard.Draw) {
+    activeCard.Draw();
+  }
+
+  buttons.forEach(b => b.EvaluateMousePosition(mouseX, mouseY));
+  factorBars.forEach(b => b.EvaluateMousePosition(mouseX, mouseY));
+
+  factorBars.forEach(b => b.Draw());
+  buttons.forEach(b => b.Draw());
+  
+}
+
+function enactChoice( choice ) {
+
+  applyEffects(activeCard, choice);
+
+  // Put active card back to bottom of deck
+  deck.AddCardToBottom(activeCard);
+
+  // Deal new card
+  activeCard = deck.DealTopCard();
+
+}
+
+function handleFile(file) {
+  
+  if (!file.name.endsWith('.csv')) {
+    console.warn("Uploaded non-CSV file.")
+    return null;
+  }
+  
+  return loadTable(
+    file.data,
+    'csv',
+    'header',
+    loadCardsFromCSV,
+    console.warn("Error loading CSV.")
+    );
+    
+  }
+
+function loadCardsFromCSV(data) {
+
+  cards = {};
+  deck.Clear();
+
+  data.rows.forEach(row => {
+
+    const card = new Card();
+
+    card.id = row.get('id');
+    card.text = row.get('text');
+
+    card.effects = {
+      'swipeYes': [],
+      'swipeNo': []
+    }
+
+    let i = 1;
+    while (row.get("yes" + i) || row.get("no" + i)) {
+      card.effects['swipeYes'].push(row.get("yes" + i));
+      card.effects['swipeNo'].push(row.get("no" + i));
+      i++;
+    }
+
+    if (!(card.id && card.text)) {
+      console.error("Invalid Card");
+      return;
+    }
+
+    if (cards[card.id]) {
+      console.error("Duplicate card ID.");
+      return;
+    }
+
+    cards[card.id] = card;
+    deck.AddCardToBottom(card);
+
+  });
+
+  if (deck.stack.length == 0 || deck.stack[0] == undefined) {
+    console.error("No cards found in CSV")
+  }
+
+  activeCard = deck.DealTopCard();
+
+}
+
+function mousePressed() {
+  buttons.forEach(button => {
+    button.EvaluateMousePress(mouseX, mouseY);
+  });
+}
+
+function previewChoice(choice) {
+  
+  let i = 0;
+  factorBars.forEach(bar => {
+    bar.SetPreview(activeCard.effects[choice][i]);
+    i++;
+  })
+
+}
 
 function setup() {
 
@@ -46,152 +201,44 @@ function setup() {
   );
 
   for (let i = 0; i < factorCount; i++) {
-    factors.push(new FactorBar(i));
+
+    const factor = new Factor({'name': factorNames[i]});
+
+    factors.push(factor);
+    factorBars.push(new FactorBar({
+      'index': i,
+      'factor': factor,
+      'previewMode': 'FULL',
+    }));
+
   }
 
   // Yes Button
   buttons.push(
     new Button({
+      'id': 'swipeYes',
       'x': width * 0.888,
       'y': height * 0.33,
       'radius': width * 0.065,
-      'colour': '#00cc00',
-      'clickEffect': () => { EnactUserChoice(true) },
+      'colour': palette['positive'],
+      'OnHover': () => { previewChoice('swipeYes'); },
+      'OnHoverExit': () => { clearPreviews() },
+      'OnClick': () => { enactChoice('swipeYes'); },
     }
     ));
 
   // No Button
   buttons.push(
     new Button({
+      'id': 'swipeNo',
       'x': width * 0.111,
       'y': height * 0.33,
       'radius': width * 0.065,
-      'colour': '#cc0000',
-      'clickEffect': () => { EnactUserChoice(false) },
+      'colour': palette['negative'],
+      'OnHover': () => { previewChoice('swipeNo'); },
+      'OnHoverExit': () => { clearPreviews(); },
+      'OnClick': () => { enactChoice('swipeNo'); },
     }
     ));
 
-}
-
-function draw() {
-
-  if (deck.stack.length === 0) {
-    background(23, 45, 67);
-    return;
-  }
-
-  background(78, 90, 123);
-
-  // Draw the active card to screen
-  if (activeCard && activeCard.Draw) {
-    activeCard.Draw();
-  }
-
-  factors.forEach(b => b.Draw());
-  buttons.forEach(b => b.Draw());
-
-}
-
-function EnactUserChoice(b) {
-
-  const boolean = Boolean(b);
-
-  ApplyEffects(activeCard, boolean);
-
-  // Put active card back to bottom of deck
-  deck.AddCardToBottom(activeCard);
-
-  // Deal new card
-  activeCard = deck.DealTopCard();
-
-}
-
-function ApplyEffects(card, boolean) {
-
-  const effects = card.effects[Boolean(boolean)];
-
-  for (let i = 0; i < effects.length; i++) {
-
-    factors[i].ApplyEffect(effects[i]);
-
-  }
-
-}
-
-function loadCardsFromCSV(data) {
-
-  cards = {};
-  deck.Clear();
-
-  data.rows.forEach(row => {
-
-    const card = new Card();
-
-    card.id = row.get('id');
-    card.text = row.get('text');
-
-    card.effects = {
-      true: [],
-      false: []
-    }
-
-    let i = 1;
-    while (row.get("yes" + i) || row.get("no" + i)) {
-      card.effects[true].push(row.get("yes" + i));
-      card.effects[false].push(row.get("no" + i));
-      i++;
-    }
-
-    if (!(card.id && card.text)) {
-      console.error("Invalid Card");
-      return;
-    }
-
-    if (cards[card.id]) {
-      console.error("Duplicate card ID.");
-      return;
-    }
-
-    cards[card.id] = card;
-    deck.AddCardToBottom(card);
-
-  });
-
-  if (deck.stack.length == 0 || deck.stack[0] == undefined) {
-    console.error("No cards found in CSV")
-  }
-
-  activeCard = deck.DealTopCard();
-
-}
-
-function handleFile(file) {
-
-  if (!file.name.endsWith('.csv')) {
-    console.warn("Uploaded non-CSV file.")
-    return null;
-  }
-
-  return loadTable(
-    file.data,
-    'csv',
-    'header',
-    loadCardsFromCSV,
-    console.warn("Error loading CSV.")
-  );
-
-}
-
-function downloadExampleCSV() {
-
-  saveTable(exampleCSV, 'swipe_game_example.csv', 'csv');
-
-}
-
-function mousePressed() {
-  buttons.forEach(button => {
-    if (button.IsWithinRange(mouseX, mouseY)) {
-      button.Click();
-    }
-  });
 }
